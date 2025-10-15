cd2f20dabc14230d928eaf6645c1ed7ab946eeb5
Roi Sulimani <roisulimani@gmail.com>
2025-10-15 13:22:21 +0300

    02 - Database Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
 
-const Page = () => {
+const Page = async () => {
+ 
   return (
   <div >
-    <Button>Click me</Button>
+    Hello
   </div>
 );
 };
