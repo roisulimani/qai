8eca3647ba9d7fabf9fac58b81084cb6039ef5bf
Roi Sulimani <roisulimani@gmail.com>
2025-10-15 15:39:51 +0300

    04 - Background Job Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
-import { Suspense } from "react";
+"use client";
 
 const Page = async () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({
-    text: "Roi",
-  }));
  
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client/>
-      </Suspense>
-      
-  </HydrationBoundary>
+    <div>
+      Test
+    </div>
 );
 };
 
