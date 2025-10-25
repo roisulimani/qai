88fff19d1fdb5a39c40837feaf25d75e0882f36f
Roi Sulimani <roisulimani@gmail.com>
2025-10-25 19:48:29 +0300

    11 - Implement project header

---
+++ src/modules/projects/ui/components/project-header.tsx
@@ -0,0 +1,100 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+    ChevronLeftIcon,
+    ChevronDownIcon,
+    EditIcon,
+    SunMoonIcon,
+} from "lucide-react";
+
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuPortal,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+
+} from "@/components/ui/dropdown-menu";
+
+
+interface Props {
+    projectId: string;
+}
+export const ProjectHeader = ({ projectId }: Props) => {
+    const trpc = useTRPC();
+    const { data: project } = useSuspenseQuery(
+        trpc.projects.getOne.queryOptions({ id: projectId })
+    );
+    const { theme, setTheme } = useTheme();
+    return (
+        <header className="p-2 flex justify-between items-center border-b">
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button 
+                        variant="ghost"
+                        size="sm"
+                        className="focus-visible:ring-0 hover:bg-transparent hover:opacity-75 
+                        transition-opacity pl-2!">
+
+                        <Image 
+                        src="/logo.png"
+                        alt="QAI"
+                        width={30}
+                        height={30}
+                        />
+                        <span className="text-sm font-medium">
+                            {project.name}
+                        </span>
+                        <ChevronDownIcon />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="bottom" align="start">
+                    <DropdownMenuItem asChild>
+                        <Link href="/">
+                            <ChevronLeftIcon/>
+                            <span>
+                                Go to Dashboard
+                            </span>
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger className="gap-2">
+                            <SunMoonIcon className="size-4 text-muted-foreground" />
+                            <span>
+                                Theme
+                            </span>
+
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                                <DropdownMenuRadioGroup value={theme} onValueChange={(value) => setTheme(value as "light" | "dark" | "system")}>
+                                    <DropdownMenuRadioItem value="light">
+                                        Light
+                                    </DropdownMenuRadioItem>
+                                    <DropdownMenuRadioItem value="dark">
+                                        Dark
+                                    </DropdownMenuRadioItem>
+                                    <DropdownMenuRadioItem value="system">
+                                        System
+                                    </DropdownMenuRadioItem>
+                                </DropdownMenuRadioGroup>
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </header>
+
+    );
+}
